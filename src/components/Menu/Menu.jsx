bdc87caf951cb8185788f75270e9be18ed91e67e
Slaven B <slaven.bunijevac@gmail.com>
2022-02-19 19:37:55 +0100

    succesfully fetch data from data.js file

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,8 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import classes from './Menu.module.css';
+import menu from '../../data';
 
 const Menu = () => {
+
+  const [ myMenu, setMyMenu ] = useState(menu);
+  
   return (
-    <div>
+    <div className={classes.menu}>
       
     </div>
   )
