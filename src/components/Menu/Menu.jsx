017ca5438dfa681ac2c227229a10fef4aa04a6b4
Slaven B <slaven.bunijevac@gmail.com>
2022-02-19 20:13:35 +0100

    import Categories into Manu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import classes from './Menu.module.css';
 import menu from '../../data';
+import Categories from '../Categories/Categories'
 
 const Menu = () => {
 
@@ -8,7 +9,8 @@ const Menu = () => {
   
   return (
     <div className={classes.menu}>
-      
+      <h2>Make your pick</h2>
+      <Categories />
     </div>
   )
 }
