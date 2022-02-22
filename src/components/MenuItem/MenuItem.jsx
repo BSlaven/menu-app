28e18d3f07ca8940b13acd89cb7b197705a52278
Slaven B <slaven.bunijevac@gmail.com>
2022-02-22 12:25:26 +0100

    import menuitem into menu component

---
+++ src/components/MenuItem/MenuItem.jsx
@@ -0,0 +1,11 @@
+import classes from './MenuItem.module.css';
+
+const MenuItem = () => {
+  return (
+    <div className={classes.item}>
+      MenuItem
+    </div>
+  )
+}
+
+export default MenuItem
