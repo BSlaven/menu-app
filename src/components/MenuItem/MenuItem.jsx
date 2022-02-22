c782a3e137249694782ecd25cc4d313d03093f59
Slaven B <slaven.bunijevac@gmail.com>
2022-02-22 12:56:47 +0100

    add all content into menu item

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -3,7 +3,13 @@ import classes from './MenuItem.module.css';
 const MenuItem = ({ item }) => {
   return (
     <div className={classes.item}>
-      <img src={item.img} alt={item.title} />
+      <div className={classes.imageContainer}>
+        <img className={classes.image} src={item.img} alt={item.title} />
+      </div>
+      <div className={classes.contentContainer}>
+        <p>{item.title} <span>{item.price}</span></p>
+        <p>{item.desc}</p>
+      </div>
     </div>
   )
 }
