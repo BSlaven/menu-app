a63e43945c8815862b0b5f70930ecdf63d05fc4c
Slaven B <slaven.bunijevac@gmail.com>
2022-02-23 23:09:47 +0100

    move propper imports after structure change

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import { useState, useEffect } from 'react';
 import classes from './App.module.css'
 import Menu from './components/Menu/Menu'
+import Categories from './components/Categories/Categories';
+import menu from './data';
 
 function App() {
+
+  const [ filterCategory, setFilterCategory ] = useState('');
+  const [ myMenu, setMyMenu ] = useState([]);
+  const categories = [...new Set(menu.map(elem => elem.category))];
+
+  const filterMenu = category => {
+    setFilterCategory(category);
+  }
+  
+  useEffect(() => {
+    if(!filterCategory) {
+      setMyMenu(menu);
+    } else {
+      const newMenu = menu.filter(elem => elem.category === filterCategory)
+      setMyMenu(newMenu);
+    }
+  }, [filterCategory]);
 
   return (
     <div className={classes.App}>
       <h2>Make your pick</h2>
       <Categories categories={categories} click={filterMenu} />
-      <Menu />
+      <Menu myMenu={myMenu} />
     </div>
   )
 }
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,28 +1,7 @@
-import { useState, useEffect } from 'react';
 import classes from './Menu.module.css';
-import menu from '../../data';
-import Categories from '../Categories/Categories';
 import MenuItem from '../MenuItem/MenuItem';
 
-const Menu = () => {
-
-  const [ filterCategory, setFilterCategory ] = useState('');
-  const [ myMenu, setMyMenu ] = useState([]);
-  const categories = [...new Set(menu.map(elem => elem.category))];
-
-  const filterMenu = category => {
-    setFilterCategory(category);
-  }
-  
-  useEffect(() => {
-    if(!filterCategory) {
-      setMyMenu(menu);
-    } else {
-      const newMenu = menu.filter(elem => elem.category === filterCategory)
-      setMyMenu(newMenu);
-    }
-  }, [filterCategory]);
-  
+const Menu = ({ myMenu }) => {  
   return (
     <div className={classes.menu}>
       {myMenu && myMenu.map(item => <MenuItem key={item.id} item={item}>{item.title}</MenuItem>)}
